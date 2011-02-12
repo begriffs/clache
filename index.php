2e63d2e9d0f8f7346371f8c3f9aa65e7ef91856e
Joe Nelson <github@begriffsschrift.com>
2011-02-12 01:29:34 +0000

    beginning to use meaningful http

--- index.php
+++ index.php
@@ -2,11 +2,17 @@
 set_time_limit(0);
 include('parse.php');
 include('reduce.php');
+header("Content-Type: text/plain");
+header("X-Powered-By: clache");
 $db = pg_connect("host=localhost dbname=clweb user=postgres password=password")
     or die('Could not connect: ' . pg_last_error());
 cl_init($db);
-?>
-<pre><?php cl_fr($db, cl_parse($_GET['cl']), 500, 1); ?></pre>
-<?php
+$t = cl_parse($_GET['cl']);
+if(!$t) {
+	header('HTTP/1.1 400 Bad Request');
+?>URL is not a well-formed combinatory logic term<?php
+	die();
+}
+cl_fr($db, cl_parse($_GET['cl']), 500, 1);
 pg_close($db);
 ?>
